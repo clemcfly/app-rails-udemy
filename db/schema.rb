8b89dd0d65f7433809a0bb3df6001aaf0a0caf5e
clemcfly <lopez.clmnt@protonmail.com>
2018-07-03 14:45:11 +0200

    ajout des validation spour les utilisateurs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180702152532) do
+ActiveRecord::Schema.define(version: 20180703123812) do
 
   create_table "utilisateurs", force: :cascade do |t|
     t.string "nom"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_utilisateurs_on_email", unique: true
   end
 
 end
--- test/models/utilisateur_test.rb
+++ test/models/utilisateur_test.rb
@@ -4,4 +4,65 @@ class UtilisateurTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @utilisateur = Utilisateur.new(nom: "Exemple Utilisateur", email: "utilisateur@exemple.com")
+  end
+
+  test "should be valid" do
+    assert @utilisateur.valid?
+  end
+
+  #nom
+
+  test "nom should be present" do
+    @utilisateur.nom = "  "
+    assert_not @utilisateur.valid?
+  end 
+
+  test "nom should not be too long" do
+    @utilisateur.nom = "a" * 51
+    assert_not @utilisateur.valid?
+  end
+
+  #email
+
+  test "email should be present" do
+    @utilisateur.email = "  "
+    assert_not @utilisateur.valid?
+  end
+
+  test "email should not be too long" do
+    @utilisateur.email = "a" * 244 + "@exemple.com"
+    assert_not @utilisateur.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    adresses_valides = %w[user@exemple.com user@foo.COM A_US-ER@foo.bar.org first.last@foo.jp justine+jean@baz.com]
+    adresses_valides.each do |adresse_valide|
+      @utilisateur.email = adresse_valide
+      assert @utilisateur.valid?, "#{adresse_valide.inspect} doit être valide."
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    adresses_invalides = %w[user@exemple,com user_at_foo.org user.name@exemple. foo@bar_baz.com foo@bar+baz.com]
+    adresses_invalides.each do |adresse_invalide|
+      @utilisateur.email = adresse_invalide
+      assert_not @utilisateur.valid?, "#{adresse_invalide.inspect} doit être invalide."
+    end
+  end
+
+  test "email addresses should be unique" do
+    utilisateur_duplique = @utilisateur.dup
+    utilisateur_duplique.email = @utilisateur.email.upcase
+    @utilisateur.save
+    assert_not utilisateur_duplique.valid?
+  end
+
+
+
+
 end
+
+
